7e222cc70c8fae123dd25abccbb744b049d07f46
trishmoyce <itsyogyrl@gmail.com>
2022-03-16 15:27:52 -0700

    button generates new grid

--- javascript.js
+++ javascript.js
@@ -1,93 +1,75 @@
-// div container for all boxes
+// main container for everything
 
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
 
 // creates class for all boxes so they can be formatted in CSS
 
-const box = document.createElement('box');
-box.classList.add('box');
+const box = document.createElement("box");
+box.classList.add("box");
 
-// creates all new boxes
+// creates a div that will appear when the page is first loaded
 
-grid01 = document.createElement('div');
-grid01.classList.add('box');
-container.appendChild(grid01);
+const initialGrid = document.createElement("intialGrid");
+initialGrid.classList.add("initialGrid");
+container.appendChild(initialGrid);
 
-grid02 = document.createElement('div');
-grid02.classList.add('box');
-container.appendChild(grid02);
+// creates all the boxes in the initial grid
 
-grid03 = document.createElement('div');
-grid03.classList.add('box');
-container.appendChild(grid03);
-
-grid04 = document.createElement('div');
-grid04.classList.add('box');
-container.appendChild(grid04);
-
-grid05 = document.createElement('div');
-grid05.classList.add('box');
-container.appendChild(grid05);
-
-grid06 = document.createElement('div');
-grid06.classList.add('box');
-container.appendChild(grid06);
-
-grid07 = document.createElement('div');
-grid07.classList.add('box');
-container.appendChild(grid07);
-
-grid08 = document.createElement('div');
-grid08.classList.add('box');
-container.appendChild(grid08);
-
-grid09 = document.createElement('div');
-grid09.classList.add('box');
-container.appendChild(grid09);
-
-grid10 = document.createElement('div');
-grid10.classList.add('box');
-container.appendChild(grid10);
-
-grid11 = document.createElement('div');
-grid11.classList.add('box');
-container.appendChild(grid11);
-
-grid12 = document.createElement('div');
-grid12.classList.add('box');
-container.appendChild(grid12);
-
-grid13 = document.createElement('div');
-grid13.classList.add('box');
-container.appendChild(grid13);
-
-grid14 = document.createElement('div');
-grid14.classList.add('box');
-container.appendChild(grid14);
-
-grid15 = document.createElement('div');
-grid15.classList.add('box');
-container.appendChild(grid15);
-
-grid16 = document.createElement('div');
-grid16.classList.add('box');
-container.appendChild(grid16);
+for (let i = 0; i < 4; i++) {
+    firstrow = document.createElement("div");
+    firstrow.classList.add("row");
+    initialGrid.appendChild(firstrow);
+        for (let i = 0; i < 4; i++) {
+            firstdiv = document.createElement("div");
+            firstdiv.classList.add("box");
+            firstrow.appendChild(firstdiv);
+}
+}
 
 // hovering causes boxes to turn pink
 
-const hover = document.querySelectorAll('.box');
+const hover = document.querySelectorAll(".box");
 for (let i = 0; i < hover.length; i++) {
-    hover[i].addEventListener('mouseover', function() {
+    hover[i].addEventListener("mouseover", function() {
         hover[i].style.backgroundColor = "pink";
 });
 }
 
-// clicking the button reverts all boxes back to lavender
+// delete the old container, replace with a new one
 
-const clearButton = document.querySelector('.clearbutton');
-const clear = document.querySelectorAll('.box');
-for (let i = 0; i < clear.length; i++) {
-    clearButton.addEventListener('click', function() {
-        clear[i].style.backgroundColor = "lavender";
-});
+const newButton = document.querySelector(".newbutton");
+
+function newGrid() {
+    var removeFirstGrid = document.querySelector(".container");
+    removeFirstGrid.firstElementChild.remove();
+    const newcontainer = document.createElement("newcontainer");
+    newcontainer.classList.add("newcontainer");
+    container.appendChild(newcontainer);
+      var answer = prompt("How many rows?");
+      if (Number.isInteger(+answer) && answer > 1 && answer <= 100) {
+        for (let i = 0; i < answer; i++) {
+          newrow = document.createElement("div");
+          newrow.classList.add("row");
+          newcontainer.appendChild(newrow);
+          for (let i = 0; i < answer; i++) {
+              newdiv = document.createElement("div");
+              newdiv.classList.add("box");
+              newrow.appendChild(newdiv);
+            }
+          }
+            const hoveragain = document.querySelectorAll(".box");
+            for (let i = 0; i < hoveragain.length; i++) {
+            hoveragain[i].addEventListener("mouseover", function() {
+            hoveragain[i].style.backgroundColor = "pink";
+            });
+            }
+        } else if (answer == 0 || answer == null) {
+          alert("Maybe next time");
+        } else if (answer >= 101) {
+          alert("Let's keep things simple");
+        } else {
+          alert("Only whole numbers, please")
+  }
 }
+
+newButton.addEventListener("click", newGrid);
